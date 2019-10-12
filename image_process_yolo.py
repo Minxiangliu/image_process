7af21b583c6c67cb0eb3d1b44db14a5290ea84fb
Minxiangliu <zzz1992012@gmail.com>
2019-10-12 20:30:06 +0800

    update

---
+++ image_process_yolo.py
@@ -0,0 +1,79 @@
+import shutil
+import time
+import sys
+import os
+import numpy as np
+import step_1
+import transferYOLO
+
+
+# This line need to change your upper layer path of image folder.
+input_path = 'D:/Program/ImageSet/'
+# This line is the output path after the data processing is completed.
+output_path = 'D:/Program/ImageSet/outputDataSet_YOLO/'
+# This line need to change your original image folder.
+DataSet_Folder = ['macncheese_DataSet']
+
+# train, test, validation
+split_rate = [0.9, 0.1]
+
+
+def check_folder(folder_path):
+	if os.path.isdir(folder_path):
+		shutil.rmtree(folder_path)
+		time.sleep(0.2)
+		os.makedirs(folder_path)
+	else:
+		os.makedirs(folder_path)
+
+
+if __name__ == '__main__':
+
+	cfg_obj_names = "obj.names"
+	cfg_obj_data = "obj.data"
+
+	if not np.sum(split_rate) == 1:
+		print('split rate error!')
+		sys.exit()
+
+	className = []
+	for f in DataSet_Folder:
+		if len(f.split('_DataSet')) == 1:
+			print('DataSet_Folder file name error!')
+			sys.exit()
+		else:
+			className.append(f.split('_DataSet')[0])
+	className = np.array(className , dtype=np.str)
+
+	step_1.file_Rename(DataSet_Folder, input_path, className)
+
+	output_dir = ['train','test','validation']
+	if os.path.isdir(output_path):
+		shutil.rmtree(output_path)
+	for i in range(len(split_rate)):
+		check_folder(output_path+output_dir[i])
+	check_folder(output_path+'cfg')
+	check_folder(output_path+'weights')
+
+
+	transferYOLO.transfer(split_rate=split_rate, 
+		input_path=input_path, output_path=output_path, 
+		DataSet_Folder=DataSet_Folder, className=className)
+
+
+	with open(output_path+'cfg/'+cfg_obj_data, 'w') as the_file:
+		the_file.write("classes = " + str(len(className)) + "\n")
+		the_file.write("train = " + output_path+'cfg/' + "train.txt\n")
+		the_file.write("valid = " + output_path+'cfg/' + "test.txt\n")
+		the_file.write("names = " + output_path+'cfg/' + "obj.names\n")
+		the_file.write("backup = " + output_path+ "weights/")
+	the_file.close()
+
+	with open(output_path+'cfg/'+cfg_obj_names, 'w') as the_file:
+		for n in className:
+			the_file.write(n + "\n")
+
+	the_file.close()
+
+
+
---
+++ transferYOLO.py
@@ -0,0 +1,123 @@
+import os
+import time
+import shutil
+import cv2
+from xml.dom import minidom
+import sys
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+def run(classList, classNumList,fileNamePath, outpath,labelGrep=""):
+
+    img = cv2.imread(fileNamePath+'.jpg')
+    if type(img) == None:
+        print('image not find!')
+        sys.exit()
+    imgShape = img.shape
+
+    img_h = imgShape[0]
+    img_w = imgShape[1]
+
+    labelXML = minidom.parse(fileNamePath+'.xml')
+    labelName = []
+    labelXmin = []
+    labelYmin = []
+    labelXmax = []
+    labelYmax = []
+
+    tmpArrays = labelXML.getElementsByTagName("filename")
+    for elem in tmpArrays:
+        filenameImage = elem.firstChild.data
+
+    tmpArrays = labelXML.getElementsByTagName("name")
+    for elem in tmpArrays:
+        labelName.append(str(elem.firstChild.data))
+
+    tmpArrays = labelXML.getElementsByTagName("xmin")
+    for elem in tmpArrays:
+        labelXmin.append(int(elem.firstChild.data))
+
+    tmpArrays = labelXML.getElementsByTagName("ymin")
+    for elem in tmpArrays:
+        labelYmin.append(int(elem.firstChild.data))
+
+    tmpArrays = labelXML.getElementsByTagName("xmax")
+    for elem in tmpArrays:
+        labelXmax.append(int(elem.firstChild.data))
+
+    tmpArrays = labelXML.getElementsByTagName("ymax")
+    for elem in tmpArrays:
+        labelYmax.append(int(elem.firstChild.data))
+
+
+    with open(outpath, 'w') as the_file:
+        i = 0
+        for className in labelName:
+            if(className==labelGrep or labelGrep==""):
+                classID = classList[className]
+                classNumList[className] += 1
+                x = (labelXmin[i] + (labelXmax[i]-labelXmin[i])/2) * 1.0 / img_w 
+                y = (labelYmin[i] + (labelYmax[i]-labelYmin[i])/2) * 1.0 / img_h
+                w = (labelXmax[i]-labelXmin[i]) * 1.0 / img_w
+                h = (labelYmax[i]-labelYmin[i]) * 1.0 / img_h
+
+                the_file.write(str(classID) + ' ' + str(x) + ' ' + str(y) + ' ' + str(w) + ' ' + str(h) + '\n')
+                i += 1
+
+    the_file.close()
+
+
+def transfer(split_rate, input_path, output_path, DataSet_Folder, className):
+    global classNumList
+
+    print('className:',className)
+
+    classList = {}
+    classNumList = {}
+    for i,c in enumerate(className):
+        classList[c] = i
+        classNumList[c] = 0
+
+
+    if len(split_rate) == 3:
+        folder_ = ['train', 'test', 'validation']
+    elif len(split_rate) == 2:
+        folder_ = ['train', 'test']
+
+
+    for p in DataSet_Folder:
+        xml_fileName = []
+        path = input_path+p+'/'
+        list_dir = os.listdir(path)
+        for l in list_dir:
+            if l.endswith('xml'):
+                xml_fileName.append(l.split('.',2)[0])
+
+        if len(folder_) == 3:
+            train, test = train_test_split(xml_fileName, test_size=split_rate[1], random_state=1)
+            train, validation = train_test_split(train, test_size=split_rate[2], random_state=1)
+            folder_parting = [train, test, validation]
+        elif len(folder_) == 2:
+            train, test = train_test_split(xml_fileName, test_size=split_rate[1], random_state=1)
+            folder_parting = [train, test]
+
+        for i, f_p in enumerate(folder_parting):
+            fileList = []
+            print(folder_[i],':',len(f_p))
+            for fileN in f_p:
+                run(classList, classNumList,path+fileN, output_path + folder_[i] +'/' + fileN +'.txt',labelGrep="")
+                shutil.copyfile(path + fileN + '.jpg', output_path + folder_[i] +'/' + fileN +'.jpg')
+                fileList.append(output_path + folder_[i] +'/' + fileN +'.jpg')
+
+            with open(output_path + 'cfg/' + folder_[i] +'.txt', 'w') as the_file:
+                for i in range(len(fileList)):
+                    the_file.write(fileList[i] + "\n")
+
+            the_file.close()
+
+            print('bbox number:',classNumList,'\n')
+            for i in classNumList:
+                classNumList[str(i)] = 0
+
+        
+        print(p,'transferYolo complete!')
